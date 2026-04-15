682c956b5d4a70fc40b151efaa62413ffbfca348
Lucas Solera <lucassolera2006@email.com>
2026-04-15 17:37:40 -0300

    Consulta simples com query methods

--- src/main/java/com/lucassolera/workshopmongo/services/PostService.java
+++ src/main/java/com/lucassolera/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lucassolera.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,9 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
