bb17cd793549ef71786895c8bfd1aee3f3316ccb
Lucas Solera <lucassolera2006@email.com>
2026-04-15 17:46:23 -0300

    Consulta Simples com @query

--- src/main/java/com/lucassolera/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucassolera/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucassolera.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucassolera.workshopmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.lucassolera.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	
+	//query methods
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
